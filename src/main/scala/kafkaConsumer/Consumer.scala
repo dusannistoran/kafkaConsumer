f12f62eb96fd4f8c46eaeaed6d055d59c52dfd56
dusannistoran <dulekiki@gmail.com>
2021-09-15 12:48:43 +0200

    commit 1

--- src/main/scala/kafkaConsumer/Consumer.scala
+++ src/main/scala/kafkaConsumer/Consumer.scala
@@ -1,39 +1,68 @@
 package kafkaConsumer
 
-import java.util
-import java.util.Properties
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
+import org.slf4j.LoggerFactory
 
-import org.apache.kafka.clients.consumer.{ConsumerConfig, KafkaConsumer}
-import org.apache.kafka.common.serialization.StringDeserializer
+class Consumer(topic: String, brokers: String) {
 
-import scala.collection.JavaConverters._
+  val spark = SparkSession
+    .builder()
+    .master("local")
+    .appName("kafkaConsumer")
+    .getOrCreate()
 
-class Consumer(topic: String, brokers: String, groupId: String) {
+  LoggerFactory.getLogger(spark.getClass)
+  spark.sparkContext.setLogLevel("WARN")
 
-  val consumer = new KafkaConsumer[String, String](configuration)
+  val mySchema = StructType(
+    StructField("id", IntegerType) ::
+      StructField("firstName", StringType) ::
+      StructField("lastName", StringType) ::
+      StructField("yearOfBirth", IntegerType) ::
+      StructField("address", StructType(
+        StructField("country", StringType) ::
+          StructField("city", StringType) ::
+          StructField("street", StringType) :: Nil
+      )) ::
+      StructField("gender", StringType) :: Nil
+  )
 
-  private def configuration: Properties = {
-    val props = new Properties()
-    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, brokers)
-    props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer].getCanonicalName)
-    props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, classOf[StringDeserializer].getCanonicalName)
-    props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId)
-    props
+  def streamJsonFromKafkaToConsole(): Unit = {
+    val df = spark
+      .readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", brokers)
+      .option("subscribe", topic)
+      .option("startingOffsets", "latest")
+      .load()
+
+    val valueDf = df.selectExpr("CAST(value AS STRING)")
+
+    import org.apache.spark.sql.functions._
+    val dfWithColumns = valueDf
+      .withColumn("value", from_json(col("value"), mySchema))
+
+    val data = dfWithColumns
+      .withColumn("id", dfWithColumns.col("value.id"))
+      .withColumn("first_name", dfWithColumns.col("value.firstName"))
+      .withColumn("last_name", dfWithColumns.col("value.lastName"))
+      .withColumn("year_of_birth", dfWithColumns.col("value.yearOfBirth"))
+      .withColumn("address_country", dfWithColumns.col("value.address.country"))
+      .withColumn("address_city", dfWithColumns.col("value.address.city"))
+      .withColumn("address_street", dfWithColumns.col("value.address.street"))
+      .drop("value.address")
+      .withColumn("gender", dfWithColumns.col("value.gender"))
+      .drop("json")
+      .drop("value")
+
+    data.writeStream
+      .format("console")
+      .outputMode("append")
+      .start()
+      .awaitTermination()
   }
 
-  def readMessages(): Unit = {
-
-    consumer.subscribe(util.Arrays.asList(topic))
-
-    while (true) {
-      val records = consumer.poll(1000).asScala
-
-      for (record <- records) {
-        println(record.value())
-      }
-    }
-
-  }
 
 }
 
@@ -41,8 +70,8 @@ object Consumer {
 
   def main(args: Array[String]): Unit = {
 
-    val consumer = new Consumer("test-topic", "localhost:9092", "test")
-    consumer.readMessages()
+    val consumer = new Consumer("test-topic", "localhost:9092")
+    consumer.streamJsonFromKafkaToConsole()
   }
 
 }
