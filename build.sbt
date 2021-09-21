2f0543ec25178ca015819233d17875f74520bfb6
dusannistoran <dulekiki@gmail.com>
2021-09-21 12:05:00 +0200

    postgres commit

--- build.sbt
+++ build.sbt
@@ -4,6 +4,8 @@ version := "0.1"
 
 val sparkVersion = "3.1.2"
 
+val postgresVersion = "42.2.13"
+
 // https://mvnrepository.com/artifact/org.apache.spark/spark-streaming
 libraryDependencies += "org.apache.spark" %% "spark-streaming" % sparkVersion
 
@@ -15,3 +17,7 @@ libraryDependencies += "org.apache.spark" %% "spark-sql" % sparkVersion
 
 // https://mvnrepository.com/artifact/org.apache.spark/spark-sql-kafka-0-10
 libraryDependencies += "org.apache.spark" %% "spark-sql-kafka-0-10" % sparkVersion
+
+// https://mvnrepository.com/artifact/org.postgresql/postgresql
+libraryDependencies += "org.postgresql" % "postgresql" % postgresVersion
+
--- src/main/scala/kafkaConsumer/Consumer.scala
+++ src/main/scala/kafkaConsumer/Consumer.scala
@@ -1,6 +1,6 @@
 package kafkaConsumer
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, Dataset, SaveMode, SparkSession}
 import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
 import org.slf4j.LoggerFactory
 
@@ -28,6 +28,7 @@ class Consumer(topic: String, brokers: String) {
       StructField("gender", StringType) :: Nil
   )
 
+  /*
   def streamJsonFromKafkaToConsole(): Unit = {
     val df = spark
       .readStream
@@ -62,7 +63,60 @@ class Consumer(topic: String, brokers: String) {
       .start()
       .awaitTermination()
   }
+  */
 
+  def streamJsonFromKafkaToPostgres(): Unit = {
+
+    val df = spark
+      .readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", brokers)
+      .option("subscribe", topic)
+      .option("startingOffsets", "latest")
+      .load()
+
+    val valueDf = df.selectExpr("CAST(value AS STRING)")
+
+    import org.apache.spark.sql.functions._
+    val dfWithColumns = valueDf
+      .withColumn("value", from_json(col("value"), mySchema))
+
+    val data = dfWithColumns
+      .withColumn("id", dfWithColumns.col("value.id"))
+      .withColumn("first_name", dfWithColumns.col("value.firstName"))
+      .withColumn("last_name", dfWithColumns.col("value.lastName"))
+      .withColumn("year_of_birth", dfWithColumns.col("value.yearOfBirth"))
+      .withColumn("address_country", dfWithColumns.col("value.address.country"))
+      .withColumn("address_city", dfWithColumns.col("value.address.city"))
+      .withColumn("address_street", dfWithColumns.col("value.address.street"))
+      .drop("value.address")
+      .withColumn("gender", dfWithColumns.col("value.gender"))
+      .drop("json")
+      .drop("value")
+
+
+    def postgresqlOptions: Map[String, String] = Map(
+      "dbtable" -> "public.people",
+      "user" -> "postgres",
+      "password" -> "pAsSwOrD",
+      "driver" -> "org.postgresql.Driver",
+      "url" -> "jdbc:postgresql://localhost:5432/people"
+    )
+
+    // write to Postgresql
+    val mode: SaveMode = SaveMode.Append
+
+    data.writeStream
+      .foreachBatch { (batch: DataFrame, _: Long) =>
+          batch.write
+            .format("jdbc")
+            .options(postgresqlOptions)
+            .mode(mode)
+            .save()
+      }
+      .start()
+      .awaitTermination()
+  }
 
 }
 
@@ -71,7 +125,7 @@ object Consumer {
   def main(args: Array[String]): Unit = {
 
     val consumer = new Consumer("test-topic", "localhost:9092")
-    consumer.streamJsonFromKafkaToConsole()
+    consumer.streamJsonFromKafkaToPostgres()
   }
 
 }
